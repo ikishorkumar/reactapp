8e80a031a497a8f1a5099900eda6e565b3f66db4
ikishorkumar <ikishorkumarofficial@gmail.com>
2020-11-14 18:14:57 +0500

    change in name

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
 				<p>
 					Edit <code>src/App.js</code> and save to reload.
 				</p>
@@ -15,7 +13,7 @@ function App() {
 					target="_blank"
 					rel="noopener noreferrer"
 				>
-					Learn React Github Action WorkFlow 🚀 Here is my app it's working 🚀
+					Learn React Git Action 🚀
 				</a>
 			</header>
 		</div>
